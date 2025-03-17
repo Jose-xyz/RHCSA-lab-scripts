3f43014aabb628ba10741efb20d223a604f579a8
jose <josenicolasosa@gmail.com>
2025-03-16 20:26:38 -0400

    review 2 final

--- 02ReviewManageUserGroups.sh
+++ 02ReviewManageUserGroups.sh
@@ -5,7 +5,7 @@
 # learner user account needs to be created
 
 
-echo -e "\e[1;31m**Run as Root user**"
+echo -e "\e[1;91m**Run as Root user**"
 
 echo " "
 
@@ -64,29 +64,67 @@ do
 done
 
 getgid=$(getent group sysadmins | awk -F: '{print $3}')
-taskgid="1550"
+taskgid=1550
 
 
 if [ "$getgid" == "$taskgid" ]; 
 then 
 	echo -e "\e[1;32m[Correct]\e[0m Gid of "sysadmins" is set to 1550"	
 else
-	echo -e "\e[1;31m[Correct]\e[0m Gid of "sysadmins" is not 1550"	
+	echo -e "\e[1;31m[Incorrect]\e[0m Gid of "sysadmins" is not 1550"	
 fi
 
 #Task 3 
 
 echo -e "\e[1;93mTask 3\e[0m"   
 
+task3cmd=$(sudo chage -l jasmine | grep -i max | awk '{ print $9 }')
+task3goal=90
+
+
+if [ "$task3cmd" == "$task3goal" ]; 
+then 
+	echo -e "\e[1;32m[Correct]\e[0m Jasmine's max password age is 90 days"	
+else
+	echo -e "\e[1;31m[Incorrect]\e[0m Jasmine's max password age is NOT 90 days"
+fi
+
+
 #Task 4 
 
 echo -e "\e[1;93mTask 4\e[0m"   
 
+task04grep1=$(sudo grep ^%sysadmins /etc/sudoers | awk '{ print $1}')
+task04grep2=$(sudo grep ^%sysadmins /etc/sudoers | awk '{ print $2}')
+task04grep3=$(sudo grep ^%sysadmins /etc/sudoers | awk '{ print $3}')
+
+
+task04p1="%sysadmins"	
+task04p2="ALL=(ALL)"	
+task04p3="NOPASSWD:"
+
+if [[ "$task04grep1" == "$task04p1" && "$task04grep2" == "$task04p2" && "$task04grep3" == "$task04p3" ]]; 
+then 
+	echo -e "\e[1;32m[Correct]\e[0m Sysadmins group super access configured"	
+else
+	echo -e "\e[1;31m[Incorrect]\e[0m Sysadmins group super access NOT configured"
+fi
 
 #Task 5  
 
 
 echo -e "\e[1;93mTask 5\e[0m"   
+
+task05cmd=$(grep PASS_MIN_DAYS /etc/login.defs | awk  'NR == 2 { print $2; exit}')
+taskgoal=30
+
+if [ "$task05cmd" == "$taskgoal" ]; 
+then 
+	echo -e "\e[1;32m[Correct]\e[0m Password minimum age configured"	
+else
+	echo -e "\e[1;31m[Incorrect]\e[0m Password minimum age is not 30 days!"
+fi
+
 
 
 #Author: Jose N. Sosa
